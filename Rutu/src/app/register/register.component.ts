daaf1274e50976e93b33b85891fe64c3ab2d835d
Sourabh Waghmare <sourabh15593@mastek.com>
2023-03-02 12:11:20 +0530

    updated

--- Rutu/src/app/register/register.component.ts
+++ Rutu/src/app/register/register.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,8 +7,6 @@ import { FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } fro
 })
 export class RegisterComponent implements OnInit {
 
-  minPw = 8;
-  formGroup: FormGroup;
 
   Departments: any = ['Accounts and Finance',
     'HR',
@@ -21,29 +18,7 @@ export class RegisterComponent implements OnInit {
     'Product development',
     'Admin department',
     'Security and transport'];
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
   ngOnInit() {
-    this.formGroup = this.formBuilder.group({
-      password: ['', [Validators.required, Validators.minLength(this.minPw)]],
-      password2: ['', [Validators.required]]
-    }, {validator: passwordMatchValidator});
   }
-  get password() { return this.formGroup.get('password'); }
-  get password2() { return this.formGroup.get('password2'); }
-
-  /* Called on each input in either password field */
-  onPasswordInput() {
-    if (this.formGroup.hasError('passwordMismatch'))
-      this.password2.setErrors([{'passwordMismatch': true}]);
-    else
-      this.password2.setErrors(null);
-  }
-
 }
-export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
-  if (formGroup.get('password').value === formGroup.get('password2').value)
-    return null;
-  else
-    return {passwordMismatch: true};
-};
-
